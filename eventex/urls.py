c56736df7e5caf06ffae08c3257513c4125d67ef
Marcio Ramos Correa <marcio.ramos.correa@gmail.com>
2013-07-27 12:08:50 -0300

    ajuste static

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,4 +17,6 @@ urlpatterns = patterns('eventex.core.views',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
 )

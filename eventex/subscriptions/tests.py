4e627f83e1b8a6c84b0e942678992e8d37ade42d
Marcio Ramos Correa <marcio.ramos.correa@gmail.com>
2013-08-01 23:27:36 -0300

    Testes, app subscriptions

---
+++ eventex/subscriptions/tests.py
@@ -0,0 +1,20 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+#coding: utf-8
+from django.test import TestCase
+
+class SubscribeTest(TestCase):
+	def setUp(self):
+		self.resp=self.client.get('/inscricao/')
+
+	def test_get(self):
+		'GET /inscricao/ must return status code 200.'
+		self.assertEqual(200, self.resp.status_code)
+
+	def test_template(self):
+		'Response should be a rendered template'
+		self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')

ba7fd6b301b5f576de7b866c4da43cff49af3c1e
Marcio Ramos Correa <marcio.ramos.correa@gmail.com>
2013-08-07 14:27:30 -0300

    habilitado admin

--- eventex/urls.py
+++ eventex/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     url(r'', include('eventex.core.urls', namespace='core')),
 )
